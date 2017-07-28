acb6de307ded4b5d9c934f5c9e7b2b5d33ab30bd
hls93 <hlshaw3@ncsu.edu>
2017-07-28 14:02:13 -0400

    fixing the issue by making two arrays

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const app = express();
 
+// =======================================
+
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
@@ -14,23 +16,53 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 
+//========================================
 
-
-const todos = [
-
-];
+const todosArray = [];
+const doneArray = [];
+ let lastID = 0;
+// =======================================
 
 app.get("/", function (req, res) {
-  res.render('home', { todos: todos });
+  res.render('home', {
+     todos: todosArray,
+     done: doneArray
+   });
 });
 
-app.post("/", function (req, res) {
-  todos.push({'todo': req.body.item, 'status': false});
+// =======================================
+
+app.post("/", function(req, res) {
+
+  lastID += 1
+
+  todosArray.push({
+    id: lastID,
+    text: req.body.item
+  });
   res.redirect('/');
 })
 
-app.post("/update", function (req, res) {
-    todos.push({'todo': req.body.item, 'status': false});
+// =======================================
+
+app.post("/:id", function (req, res) {
+
+  let id = req.params.id;
+
+  holding = todosArray.filter(function(li) {
+    return li.id == id;
+  });
+
+  holding.forEach(function(li) {
+
+    let index = todosArray.indexOf(li);
+    if (index != -1){
+      todosArray.splice(index, 1);
+    }
+
+    doneArray.push(li);
+  })
+
     res.redirect('/');
 });
 
